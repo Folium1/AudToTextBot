d39839840764c78bbd88e675c092b0b242dd8340
Folium1 <vampirowitch.vampir@gmail.com>
2023-04-09 23:18:47 +0300

    Created service layer for redis db

---
+++ service/redisService.go
@@ -0,0 +1,91 @@
+package service
+
+import (
+	"errors"
+	"tgbot/storage"
+)
+
+var (
+	ErrUserDoesntExist = errors.New("User doesn't exist")
+	ErrMaxTimeExceeded = errors.New("Max time exceeded")
+	PremiumMaxTime     = 3600
+	UnpremiumMaxTime   = 480
+)
+
+type RedisService struct {
+	storage storage.RedisStorage
+}
+
+func NewRedisService() (*RedisService, error) {
+	redisStorage, err := storage.NewStorage()
+	if err != nil {
+		return nil, err
+	}
+	return &RedisService{
+		storage: *redisStorage,
+	}, nil
+}
+
+func (rs *RedisService) IcrementPremiumTime(userId, duration int) (int, error) {
+	time, err := rs.storage.IncrementPremiumTime(userId, duration)
+	if err != nil {
+		return 0, err
+	}
+	if time >= PremiumMaxTime {
+		return time, ErrMaxTimeExceeded
+	}
+	return time, nil
+}
+
+func (rs *RedisService) GetPremiumTime(userId int) (int, error) {
+	time, err := rs.storage.GetPremiumTime(userId)
+	if err != nil {
+		return 0, err
+	}
+	return time, nil
+}
+
+func (rs *RedisService) SaveUnpremiumUser(userId int) error {
+	err := rs.storage.SaveUnpremiumUser(userId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (rs *RedisService) SavePremiumUser(userId int) error {
+	return rs.storage.SavePremiumUser(userId)
+}
+
+func (rs *RedisService) IsPremium(userId int) (bool, error) {
+	isPremium, err := rs.storage.IsPremium(userId)
+	if err != nil {
+		return false, err
+	}
+	return isPremium == 1, nil
+}
+
+func (rs *RedisService) GetUnpremiumTimeSpent(userId int) (int, error) {
+	timeSpent, err := rs.storage.GetUnpremiumTime(userId)
+	if err != nil {
+		return 0, err
+	}
+	if timeSpent == -1 {
+		return 0, ErrUserDoesntExist
+	}
+	if timeSpent >= UnpremiumMaxTime {
+		return 0, ErrMaxTimeExceeded
+	}
+	return timeSpent, nil
+}
+
+func (rs *RedisService) IncrementUnpremiumTime(userId int, audioDuration int) (int, error) {
+	time, err := rs.storage.IncrementUnpremiumTime(userId, audioDuration)
+	if err != nil {
+		return 0, err
+	}
+	if time >= UnpremiumMaxTime {
+		return time, ErrMaxTimeExceeded
+	}
+	return time, nil
+}
