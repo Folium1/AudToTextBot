efe4921fb9663f7d52c3bd0941ef5f0a8d5d96b1
Vlad <vampirowitch.vampir@gmail.com>
2023-06-25 16:20:40 +0300

    Added init() func

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"tgbot/bot"
+	"fmt"
+	"log"
+	tgbot "tgbot/audTextBot"
+
+	"github.com/joho/godotenv"
 )
 
+// Init initializes the environment variables
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(fmt.Sprint("Couldn't load local variables, err:", err))
+	}
+}
+
 func main() {
-	bot.StartBot()
+	tgbot.StartBot()
 }
